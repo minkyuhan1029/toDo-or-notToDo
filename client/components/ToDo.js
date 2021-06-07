3b3794289a5c49c79eb9510fcf80685e469f5d7e
Min Kyu Han <minkyu.han1029@gmail.com>
2021-06-07 11:50:14 -0400

    features-completed

--- client/components/ToDo.js
+++ client/components/ToDo.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import SingleToDo from './SingleToDo'
+import FilterButton from './FilterButton'
 import {connect} from 'react-redux'
 import {fetchErrands, addErrandDB, updateErrandDB, deleteErrandDB} from '../store/errand'
+
+const filterOpt = {
+  All: () => true,
+  Active: errand => !errand.isDone,
+  Completed: errand => errand.isDone
+}
 
 class ToDo extends React.Component{
   constructor(){
@@ -9,15 +16,16 @@ class ToDo extends React.Component{
     this.state = {
       content: '',
       isDone: false,
+      filter: 'All',
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleEnter = this.handleEnter.bind(this)
     this.handleDone = this.handleDone.bind(this)
     this.handleDelete = this.handleDelete.bind(this)
+    this.setFilter=this.setFilter.bind(this)
   }
   componentDidMount(){
     this.props.fetchErrands();
-    this.props.fetchCount();
   }
 
   handleChange(event){
@@ -42,15 +50,35 @@ class ToDo extends React.Component{
   numOfTasks(){
     return this.props.errand.all.filter((errand)=>!errand.isDone).length
   }
+  setFilter(string){
+    this.setState({
+        filter: string
+      })
+  }
+  handleAllDone(){
+    this.props.errand.all.map((errand)=>this.handleDone(errand.id, {'isDone' : !this.state.isDone}))
+    this.setState({
+      isDone: !this.state.isDone
+    })
+  }
+
 
   render(){
+    const filterButtons = Object.keys(filterOpt).map(filter => (<FilterButton key={filter} name={filter} setFilter={this.setFilter} pressed={filter===this.state.filter}/>))
     return(
       <div className='container'>
+        <button type='button' className='chevron' onClick= {()=>this.handleAllDone()}> Chevron </button>
         <input type='text' value = {this.state.content} onChange = {this.handleChange} onKeyDown = {this.handleEnter}/>
         <div className='todo-list'>
-          {this.props.errand.all.map((errand) => <SingleToDo key = {errand.id} err={errand} handleDone = {this.handleDone} handleDelete = {this.handleDelete}/> )}
+          {this.props.errand.all.filter(filterOpt[this.state.filter]).map((errand) => <SingleToDo key = {errand.id} err={errand} handleDone = {this.handleDone} handleDelete = {this.handleDelete} updateErrand={this.props.updateErrand}/> )}
         </div>
-        <div>{this.numOfTasks()} {this.numOfTasks() > 1 ? 'errands' : 'errand'} left</div>
+        <div id = 'counter'>{this.numOfTasks()} {this.numOfTasks() > 1 ? 'errands' : 'errand'} left</div>
+        <div className ='filters'>
+          {filterButtons}
+        </div>
+        <div className='clear-completed'>
+          {this.props.errand.all.filter((errand)=> errand.isDone).length > 0 ? <button type="button" onClick={()=>this.props.errand.all.filter((errand)=>errand.isDone).map((errand)=>this.handleDelete(errand.id))}>Contains</button> : <div> </div>}
+        </div>
       </div>
     )
   }
@@ -67,6 +95,5 @@ const mapDispatch = dispatch => ({
   addErrand: (errand) => dispatch(addErrandDB(errand)),
   updateErrand: (id, update) => dispatch(updateErrandDB(id, update)),
   deleteErrand: (id) => dispatch(deleteErrandDB(id)),
-  fetchCount: () => dispatch(fetchCount())
 })
 export default connect(mapState,mapDispatch)(ToDo)
